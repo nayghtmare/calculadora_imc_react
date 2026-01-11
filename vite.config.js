838605bcee7b9d01e3650617fa17781bc81624fc
Nayara Stelmach <117122781+nayghtmare@users.noreply.github.com>
2026-01-11 19:25:12 -0300

    Add files via upload
    firstCommit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_GH_PAGES === 'true' ? '/calculadora_imc_react/' : '/',
+})
